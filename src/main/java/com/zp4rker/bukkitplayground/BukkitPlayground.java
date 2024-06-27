bd6cccf28d38b4a5420dd0e66dc1ba3172b260d1
zp4rker <iam@zp4rker.com>
2024-06-28 09:33:32 +1000

    Create plugin

---
+++ src/main/java/com/zp4rker/bukkitplayground/BukkitPlayground.java
@@ -0,0 +1,12 @@
+package com.zp4rker.bukkitplayground;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BukkitPlayground extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("Initialising...");
+    }
+
+}
