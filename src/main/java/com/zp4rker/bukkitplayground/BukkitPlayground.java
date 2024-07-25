fea44ee7d394d71bb091fb8c8be5962f44185ab5
Zaeem Parker <iam@zp4rker.com>
2024-07-25 10:24:10 +1000

    Cleanup main class

--- src/main/java/com/zp4rker/bukkitplayground/BukkitPlayground.java
+++ src/main/java/com/zp4rker/bukkitplayground/BukkitPlayground.java
@@ -3,10 +3,8 @@ package com.zp4rker.bukkitplayground;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class BukkitPlayground extends JavaPlugin {
-
     @Override
     public void onEnable() {
-        getLogger().info("Initialising...");
+        // TODO: add registering code
     }
-
 }
